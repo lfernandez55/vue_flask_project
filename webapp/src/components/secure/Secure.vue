79fe59411ceb308ee1f022e01972988e45ad35ea
lfernandez55 <lfernandez@weber.edu>
2020-09-26 18:24:24 -0600

    Update Secure.vue,  Shows token.  Makes request to api/resource.  Returns and displays api msg

--- webapp/src/components/secure/Secure.vue
+++ webapp/src/components/secure/Secure.vue
@@ -1,27 +1,60 @@
 <template>
-    <div id="secure">
-        <h1>Secure Area</h1>
-        Token: {{this.$store.state.auth.token}}
-        <p>
-            This is a secure area
-        </p>
-    </div>
+  <div id="secure">
+    <h1>Secure Area</h1>
+    <p>Token: {{ this.$store.state.auth.token }}</p>
+    <p>Token with getter: {{ this.$store.getters.token }}</p>
+    <p>Message from server: {{serverMsg}}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Secure',
-        data() {
-            return {};
-        }
-    }
+export default {
+  name: "Secure",
+  data() {
+    return {
+      userName: "",
+      serverMsg: "",
+    };
+  },
+  mounted: function() {
+    alert("dddddd");
+    this.getData();
+  },
+  methods: {
+    getData() {
+      let headerObj = {
+        headers: {
+          "Content-Type": "text/plain",
+          Authorization:
+            "Basic " + btoa(this.$store.state.auth.token + ":" + "whatever"),
+        },
+      };
+      this.$http
+        .get("http://127.0.0.1:5000/api/resource", headerObj)
+        .then((response) => response.json())
+        .then((resp) => {
+          if (resp) {
+            console.log(resp);
+            this.serverMsg = resp.data;
+          }
+        })
+        .catch((err) => {
+          if (err.body.error == "Unauthorized access") {
+            this.msg = err.body.error;
+          } else {
+            console.log("Error: " + err.message);
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-    #secure {
-        background-color: #FFFFFF;
-        border: 1px solid #CCCCCC;
-        padding: 20px;
-        margin-top: 10px;
-    }
+#secure {
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
+  padding: 20px;
+  margin-top: 10px;
+}
 </style>
