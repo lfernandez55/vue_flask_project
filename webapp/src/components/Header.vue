021b2c6f10fbf733fa6a48b1282e84a2fa534ad6
lfernandez55 <lfernandez@weber.edu>
2020-10-06 16:18:28 -0600

    Breadcrumbs added.

--- webapp/src/components/Header.vue
+++ webapp/src/components/Header.vue
@@ -1,23 +1,22 @@
 <template>
+<div>
   <nav class="navbar navbar-default">
     <div class="container-fluid">
       <!-- <transition name="slide" mode="out-in"> -->
-      <transition name="fade" > 
-        <div id="flash" v-if="this.$store.state.general.displayAlert"   v-bind:class="classObject" role="alert">
-            {{ this.$store.state.general.alertMsg }}
+      <transition name="fade">
+        <div
+          id="flash"
+          v-if="this.$store.state.general.displayAlert"
+          v-bind:class="classObject"
+          role="alert"
+        >
+          {{ this.$store.state.general.alertMsg }}
         </div>
       </transition>
       <div class="navbar-header">
         <router-link to="/" class="navbar-brand">Vue-Flask App</router-link>
       </div>
-      <!-- {{$store.state.general.alertMsg}}
-      {{this.$store.state.general.alertType}} -->
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <router-link tag="li" to="/secure" active-class="active"
-            ><a>Secure</a></router-link
-          >
-        </ul>
+      <div>
         <ul class="nav navbar-nav navbar-right">
           <li v-show="this.$store.getters.authenticated">
             <a href="#" @click="profile">{{ this.$store.state.auth.email }}</a>
@@ -29,46 +28,72 @@
       </div>
     </div>
   </nav>
+  <nav aria-label="breadcrumb">
+    <breadcrumbs />
+  </nav>
+  </div>
 </template>
 
 <script>
+import Breadcrumbs from './public/Breadcrumbs.vue'
+
 export default {
+  components: {
+    Breadcrumbs
+  },
   data() {
     return {
       isDropdownOpen: false,
-      toggle: false
+      toggle: false,
     };
   },
   computed: {
-    classObject: function () {
+    classObject: function() {
       return {
-        'alert': true,
-        'alert-success': this.$store.state.general.alertType == "success",
-        'alert-danger': this.$store.state.general.alertType == "danger" ||
-        (this.$store.state.general.alertType != "success" 
-        && this.$store.state.general.alertType != "danger" 
-        && this.$store.state.general.alertType != "info"),
-        'alert-info': this.$store.state.general.alertType == "info"
-      }
+        alert: true,
+        "alert-success": this.$store.state.general.alertType == "success",
+        "alert-danger":
+          this.$store.state.general.alertType == "danger" ||
+          (this.$store.state.general.alertType != "success" &&
+            this.$store.state.general.alertType != "danger" &&
+            this.$store.state.general.alertType != "info"),
+        "alert-info": this.$store.state.general.alertType == "info",
+      };
+    },
+        crumbs: function() {
+      let pathArray = this.$route.path.split("/")
+      pathArray.shift()
+      let breadcrumbs = pathArray.reduce((breadcrumbArray, path, idx) => {
+        breadcrumbArray.push({
+          path: path,
+          to: breadcrumbArray[idx - 1]
+            ? "/" + breadcrumbArray[idx - 1].path + "/" + path
+            : "/" + path,
+          text: this.$route.matched[idx].meta.breadCrumb || path,
+        });
+        return breadcrumbArray;
+      }, [])
+      return breadcrumbs;
     }
   },
   // this is needed for the fade effect
   watch: {
     "$store.state.general.displayAlert"(val) {
-        if (val===true){
-          setTimeout(()=>this.$store.state.general.displayAlert=false,5000);
-        } 
-        
-    }
-  }, 
+      if (val === true) {
+        setTimeout(
+          () => (this.$store.state.general.displayAlert = false),
+          5000
+        );
+      }
+    },
+  },
   methods: {
     logOut: function() {
       this.$store.dispatch("setToken", "");
     },
     profile: function() {
       this.$router.replace({ name: "profile" });
-    }
-
+    },
   },
 };
 </script>
@@ -85,11 +110,12 @@ export default {
   top: 40px;
   z-index: 10;
 }
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 1.3s ease;
 }
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
-
 </style>
--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -7,11 +7,25 @@ import Profile from './components/profile/Profile.vue'
 import Debug from './components/public/Debug.vue'
 
 export const routes = [
-  {path: '/', component: Login, name: 'login'},
+  {path: '/', component: Login, name: 'login',meta: {
+    breadcrumb: [
+      { name: 'Home' }
+    ]
+  }},
   {path: '/portfolio', component: Portfolio, name: 'portfolio'},
   {path: '/stocks', component: Stocks, name: 'stocks'},
   // {path: '/login', component: Login, name: 'login'},
-  {path: '/secure', component: Secure, name: 'secure'},
-  {path: '/profile', component: Profile, name: 'profile'},
+  {path: '/secure', component: Secure, name: 'secure',      meta: {
+    breadcrumb: [
+      { name: 'Home', link: '/' },
+      { name: 'Secure' }
+    ]
+  }},
+  {path: '/profile', component: Profile, name: 'profile',      meta: {
+    breadcrumb: [
+      { name: 'Home', link: '/' },
+      { name: 'Profile' }
+    ]
+  }},
   {path: '/debug', component: Debug, name: 'debug'}
 ];
